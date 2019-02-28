688e914af18608a6a03c8d22c9c920872d2e2b37
lguyot <luc.guyot@epfl.ch>
2019-02-28 16:58:02 +0100

    Second layout: requirements have been included in setup.py

--- eplfx_robox_nrp_utils/eplfx_robox_nrp_utils/version.py
+++
@@ -1,7 +0,0 @@
-"""
-This module contains the package version number
-"""
-
-__author__ = "Ihor Kuras"
-
-VERSION = "0.0.1"
--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
-'''setup.py'''
-# pylint: disable=R0801
+import setuptools
 import os
-
-from setuptools import setup
 from optparse import Option
-
-import epflx_robox_nrp_utils
 import pip
-
 
 def parse_reqs(reqs_file):
     ''' parse the requirements '''
     options = Option("--workaround")
     options.skip_requirements_regex = None
     # Hack for old pip versions
-    if pip.__version__.startswith('10.'):
+    major = int(pip.__version__.split('.')[0])
+    if major >= 10:
         # Versions greater or equal to 10.x don't rely on pip.req.parse_requirements
         install_reqs = list(val.strip() for val in open(reqs_file))
         reqs = install_reqs
-    elif pip.__version__.startswith('1.'):
+    elif major ==  1:
         # Versions 1.x rely on pip.req.parse_requirements
         # but don't require a "session" parameter
         from pip.req import parse_requirements # pylint:disable=no-name-in-module, import-error
@@ -41,30 +36,23 @@ def parse_reqs(reqs_file):
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 REQS = parse_reqs(os.path.join(BASEDIR, 'requirements.txt'))
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-EXTRA_REQS_PREFIX = 'requirements_'
-EXTRA_REQS = {}
-for file_name in os.listdir(BASEDIR):
-    if not file_name.startswith(EXTRA_REQS_PREFIX):
-        continue
-    base_name = os.path.basename(file_name)
-    (extra, _) = os.path.splitext(base_name)
-    extra = extra[len(EXTRA_REQS_PREFIX):]
-    EXTRA_REQS[extra] = parse_reqs(file_name)
-
-config = {
-    'description': 'Functions for the implementation of the SOM and SARSA algorithms (EPFL NRP MOOC)',
-    'author': 'HBP neurorobotics',
-    'url': 'http://neurorobotics.net',
-    'author_email': 'neurorobotics@humanbrainproject.eu',
-    'version': epflx_robox_nrp_utils.__version__,
-    'install_requires': REQS,
-    'extras_require': EXTRA_REQS,
-    'packages': ['epflx_robox_nrp_utils.',
-                 'epflx_robox_nrp_utils.SARSA', 'epflx_robox_nrp_utils.SOM'],
-    'scripts': [],
-    'name': 'epflx-robox-nrp-utils',
-    'include_package_data': True,
-}
-
-setup(**config)
+setuptools.setup(
+    name="epflx_robox_nrp_utils",
+    version="0.0.1",
+    author="Ihor Kuras",
+    author_email="ihor.kuras@epfl.ch",
+    description="Functions and classes for SOM and SARSA algorithms (EPFLx-Robox-Neurorobotics)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HBPNeurorobotics/EPFLx-RoboX-Neurorobotics-utils",
+    packages=setuptools.find_packages(),
+    install_requires=REQS,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
