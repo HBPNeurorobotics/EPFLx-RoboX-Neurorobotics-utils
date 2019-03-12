01e2c87e3262af65452fdc4251f8134c952a7910
Ihor Kuras <ihor.kuras@epfl.ch>
2019-03-12 20:11:16 +0100

    Update SARSA_additional.py

--- epflx_robox_nrp_utils/SARSA/SARSA_additional.py
+++ epflx_robox_nrp_utils/SARSA/SARSA_additional.py
@@ -643,8 +643,22 @@ class SARSA_additional():
 		#print
 		#print "Heat:\n", 
 		#print heatmap
-		df = pd.DataFrame(heatmap); df.columns.name = 'Q';
-		cm = sns.light_palette("green", as_cmap=True)
-		df = df.style.background_gradient(cmap=cm)
-		display.display(df)
+
+		
+		import pandas as pd
+		import matplotlib.pyplot as plt
+		from matplotlib import colors
+
+		def background_gradient(s, m, M, cmap='PuBu', low=0, high=0):
+			rng = M - m
+			norm = colors.Normalize(m - (rng * low),
+			M + (rng * high))
+			normed = norm(s.values)
+			c = [colors.rgb2hex(x) for x in plt.cm.get_cmap(cmap)(normed)]
+			return ['background-color: %s' % color for color in c]
 				
+		df = pd.DataFrame(heatmap); df.columns.name = 'Q';
+		#cm = sns.light_palette("green", as_cmap=True)
+		#df = df.style.background_gradient(cmap=cm)
+		df = df.style.apply(background_gradient, cmap='PuBu', m=df.min().min(), M=df.max().max(),low=0,high=0.2)
+		display.display(df)
