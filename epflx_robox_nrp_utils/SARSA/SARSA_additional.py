3805f5a4d478178b5ddd3e8c02c4c4c6580d2acb
Ihor Kuras <ihor.kuras@epfl.ch>
2019-03-12 16:32:43 +0100

    Update SARSA_additional.py

--- epflx_robox_nrp_utils/SARSA/SARSA_additional.py
+++ epflx_robox_nrp_utils/SARSA/SARSA_additional.py
@@ -593,16 +593,16 @@ class SARSA_additional():
 		#print "Right:\n", reward[:,:,2]
 		#print "Left:\n",  reward[:,:,3]
 		df = pd.DataFrame(reward[:,:,0]); df.columns.name = 'DOWN:...'; 
-		df = df.style.applymap(color_negative_red).apply(highlight_max); display.display(df)
+		df = df.style.applymap(color_negative_red).apply(highlight_max).set_properties(**{'width': '100px'}); display.display(df)
 		
 		df = pd.DataFrame(reward[:,:,1]); df.columns.name = 'UP:.....'; 
-		df = df.style.applymap(color_negative_red).apply(highlight_max); display.display(df)
+		df = df.style.applymap(color_negative_red).apply(highlight_max).set_properties(**{'width': '100px'}); display.display(df)
 		
 		df = pd.DataFrame(reward[:,:,2]); df.columns.name = 'RIGHT:..'; 
-		df = df.style.applymap(color_negative_red).apply(highlight_max); display.display(df)
+		df = df.style.applymap(color_negative_red).apply(highlight_max).set_properties(**{'width': '100px'}); display.display(df)
 		
 		df = pd.DataFrame(reward[:,:,3]); df.columns.name = 'LEFT:...'; 
-		df = df.style.applymap(color_negative_red).apply(highlight_max); display.display(df)
+		df = df.style.applymap(color_negative_red).apply(highlight_max).set_properties(**{'width': '100px'}); display.display(df)
 		
 		raw_input('Reward matrix is generated for your maze.\n\nPress Enter to finish... ')
 		clear_output()
