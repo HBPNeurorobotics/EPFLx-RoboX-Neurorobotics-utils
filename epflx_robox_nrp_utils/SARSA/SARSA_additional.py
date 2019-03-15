4cfbad04dfe4583f42906c876f88e112acca2a64
Ihor Kuras <ihor.kuras@epfl.ch>
2019-03-15 14:05:47 +0100

    Update SARSA_additional.py

--- epflx_robox_nrp_utils/SARSA/SARSA_additional.py
+++ epflx_robox_nrp_utils/SARSA/SARSA_additional.py
@@ -683,7 +683,7 @@ class SARSA_additional():
 			rng = M - m
 			norm = colors.Normalize(m - (rng * low),
 			M + (rng * high))
-			s.at[0,0] = 1000.0
+			s.at[0,'0'] = 1000.0
 			normed = norm(s.values)
 			c = [colors.rgb2hex(x) for x in plt.cm.get_cmap(cmap)(normed)]
 			return ['background-color: %s' % color for color in c]
