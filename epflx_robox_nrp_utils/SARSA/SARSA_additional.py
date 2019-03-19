a76eca3426a34925be0f25d01f8c0b93a3fd33f2
Ihor Kuras <ihor.kuras@epfl.ch>
2019-03-19 19:57:47 +0100

    Update SARSA_additional.py

--- epflx_robox_nrp_utils/SARSA/SARSA_additional.py
+++ epflx_robox_nrp_utils/SARSA/SARSA_additional.py
@@ -820,7 +820,8 @@ class SARSA_additional():
 		#heatmap[goal[0],goal[1]] = math.inf
 		#print actions
 		
-		print np.dstack((heatmap2,actions))
+		combine = np.dstack((heatmap2,actions))
+		print combine[0][1]
 		
 		df = pd.DataFrame(heatmap); df.columns.name = 'Q';
 		df = df.style.applymap(color_negative).apply(background_gradient, cmap='PuBu', m=df.min().min(), M=df.max().max(),low=0,high=0.2, goal=goal).set_properties(**{'width': '100px', 'border': '3px 1px black solid !important',  'color': 'black !important'});
