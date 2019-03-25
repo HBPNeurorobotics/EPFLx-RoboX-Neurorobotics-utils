688000c4062fc23e274dfa661ecc39e89890a855
Ihor Kuras <ihor.kuras@epfl.ch>
2019-03-25 10:45:31 +0100

    Update SARSA_additional.py

--- epflx_robox_nrp_utils/SARSA/SARSA_additional.py
+++ epflx_robox_nrp_utils/SARSA/SARSA_additional.py
@@ -789,7 +789,7 @@ class SARSA_additional():
 			normed = norm(s.values)
 			print normed
 			print normed.min().min()
-			for x in plt.cm.get_cmap(cmap)(normed): print x
+			for x in plt.cm.get_cmap(cmap)(normed): print "XX", x
 			c = [colors.rgb2hex(x) for x in plt.cm.get_cmap(cmap)(normed)]
 			#cb = [colors.rgb2hex(x) for x in plt.cm.get_cmap(cmap)(0.0)]
 			#print cb
