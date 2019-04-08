132651e234368c38a892d89e0d993530dbeb5019
Ihor Kuras <ihor.kuras@epfl.ch>
2019-04-08 14:37:08 +0200

    Update SARSA_autograduation.py

--- epflx_robox_nrp_utils/graduation/SARSA_autograduation.py
+++ epflx_robox_nrp_utils/graduation/SARSA_autograduation.py
@@ -89,7 +89,7 @@ class SARSA_autograduation():
 
 			# upload solution
 			try:
-				SARSA = self.upload_solution(func,True); load = True
+				SARSA = self.upload_solution(func,True); load = True; print "Upload done"
 			except: sumwayF = 0.0; os.chdir('..'); load = False; print "Upload failed"
 
 			if(load):
