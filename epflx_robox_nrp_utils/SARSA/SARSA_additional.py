759b673675388005f3b596fb4df3a1b64a982c98
Ihor Kuras <ihor.kuras@epfl.ch>
2019-04-09 14:21:37 +0200

    Update SARSA_additional.py

--- epflx_robox_nrp_utils/SARSA/SARSA_additional.py
+++ epflx_robox_nrp_utils/SARSA/SARSA_additional.py
@@ -283,6 +283,7 @@ class SARSA_additional():
 				output[i][j] = '['+','.join(str(e) for e in reward[i*self.Nn+j])+']'
 				if(i==self.s_goal[0] and j==self.s_goal[1]): output[i][j] = ' ' + output[i][j]
 				
+		print "self.test: ", self.testfile
 		if(self.testfile=='SOM_data_lattice.csv'): print 'Possible actions to choose: 0 - Down; 1 - Up; 2 - Right; 3 - Left.'
 		df = pd.DataFrame(output); df.columns.name = 'Actions';
 		df.to_csv('SOM_possible_actions.csv')
