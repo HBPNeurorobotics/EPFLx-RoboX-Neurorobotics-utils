dbeba9097ce83e65eca874ca5f4829b4158b7ac4
Ihor Kuras <ihor.kuras@epfl.ch>
2019-04-09 13:28:20 +0200

    Update SARSA_evaluation.py

--- epflx_robox_nrp_utils/SARSA/SARSA_evaluation.py
+++ epflx_robox_nrp_utils/SARSA/SARSA_evaluation.py
@@ -14,25 +14,25 @@ import time
 
 class SARSA_evaluation():
     
-    def __init__(self):
+	def __init__(self):
         
-        import warnings; warnings.filterwarnings('ignore')
-        from SARSA_additional import SARSA_additional
-        self.sarsaad = SARSA_additional()
+        	import warnings; warnings.filterwarnings('ignore')
+        	from SARSA_additional import SARSA_additional
+        	self.sarsaad = SARSA_additional()
 
-        self.Nn = {}
-        self.Q = {}
-        self.actions = {}
-        
-        self.centers = {}
-        self.edges = {}
+		self.Nn = {}
+		self.Q = {}
+		self.actions = {}
+
+		self.centers = {}
+		self.edges = {}
             
     
 	################################################################################
 	# Modes
 	################################################################################
 
-    def run_evaluation(self,video):
+	def run_evaluation(self,video):
 		self.Nn,self.states,self.actions,self.reward,self.goal,self.Q = self.sarsaad.eva_analysis()
 		self.M = self.perfect_map()
 		self.video = video
@@ -58,7 +58,7 @@ class SARSA_evaluation():
 
 
 
-    def test_generation(self):
+	def test_generation(self):
 		self.Nn,self.states,self.actions,self.reward,self.goal,self.Q = self.sarsaad.eva_analysis()
 		self.M = self.perfect_map()
 		self.video = 1; self.test = False
@@ -98,123 +98,67 @@ class SARSA_evaluation():
 			for i in range(len(self.X)):
 				writer.writerow([self.X[i], self.Y[i]])
 		
-    
-    def auto_generation(self):
+    	"""
+	def auto_generation(self):
 		self.Nn,self.states,self.actions,self.reward,self.goal,self.Q = self.sarsaad.eva_analysis()
 		self.M = self.perfect_map()
 		return self.M
-
+	"""
 
 	################################################################################
 	# additional to Modes
 	################################################################################
 
-    def trial_evaluation(self):
-        Qway = []
-        state = [self.start[0],self.start[1]]
-        self.X = [self.start[0]]; self.Y = [self.start[1]];
+	def trial_evaluation(self):
+		Qway = []
+		state = [self.start[0],self.start[1]]
+		self.X = [self.start[0]]; self.Y = [self.start[1]];
 
-        while(state != self.goal) and (len(Qway) < self.Nn*self.Nn):
+		while(state != self.goal) and (len(Qway) < self.Nn*self.Nn):
+			try: index = np.argmax(self.Q[state[0],state[1],:])
+			except: break
+			
+			Qway.append(self.Q[state[0],state[1],index])
+			if  (index == 0): state[0] += 1
+			elif(index == 1): state[0] -= 1
+			elif(index == 2): state[1] += 1
+			else:             state[1] -= 1
 
-            try: index = np.argmax(self.Q[state[0],state[1],:])
-            except: break
-            Qway.append(self.Q[state[0],state[1],index])
-            if  (index == 0): state[0] += 1
-            elif(index == 1): state[0] -= 1
-            elif(index == 2): state[1] += 1
-            else:             state[1] -= 1
+			self.X.append(state[0]); self.Y.append(state[1])
+			if(self.video): self.visualizationE(self.states,self.actions)
+		return Qway
 
-            self.X.append(state[0]); self.Y.append(state[1])
-            if(self.video): self.visualizationE(self.states,self.actions)
-        return Qway
-
-    """
-    def perfect_map(self):
+	def perfect_map(self):
 		N = self.Nn*self.Nn-1
 		M = np.zeros((self.Nn,self.Nn))+100
-		M[self.goal[0],self.goal[1]] = -1
+		M[self.goal[0],self.goal[1]] = 0
+
 		t = 0; L = 0
 		while N > L:
 			t += 1
 			for i in range(self.Nn):
 				for j in range(self.Nn):
-				    ways = []
-				    try: ways.append(self.reward[(i+1)*self.Nn+j][1])
-				    except: n = 1
-				    try: ways.append(self.reward[(i-1)*self.Nn+j][0]) 
-				    except: n = 1
-				    try: ways.append(self.reward[i*self.Nn+(j+1)][3])
-				    except: n = 1
-				    try: ways.append(self.reward[i*self.Nn+(j-1)][2])
-				    except: n = 1
-
-				    if(np.max(ways) < 0) and (t==1): L += 1
-				    for d in range(len(self.reward[i*self.Nn+j])):
-				        if(self.reward[i*self.Nn+j][d] == 1):
-				            if(M[i,j]==100): N = N - 1
-				            M[i,j] = 1; 
-				        elif(self.reward[i*self.Nn+j][d] == 0):     
-				            if(d==0):
-				                if(M[i+1,j]+1<M[i,j]): 
-				                    if(M[i,j]==100): N = N - 1
-				                    M[i,j] = M[i+1,j] + 1;
-				            if(d==1):
-				                if(M[i-1,j]+1<M[i,j]): 
-				                    if(M[i,j]==100): N = N - 1
-				                    M[i,j] = M[i-1,j] + 1;
-				            if(d==2):
-				                if(M[i,j+1]+1<M[i,j]): 
-				                    if(M[i,j]==100): N = N - 1
-				                    M[i,j] = M[i,j+1] + 1; 
-				            if(d==3):
-				                if(M[i,j-1]+1<M[i,j]): 
-				                    if(M[i,j]==100): N = N - 1
-				                    M[i,j] = M[i,j-1] + 1; 
-
-
-		M[self.goal[0],self.goal[1]] = 100
-		self.M = np.where(M==100, 0, M)
-		print self.M
-		return self.M
-	
-	"""
-    def perfect_map(self):
-	N = self.Nn*self.Nn-1
-	M = np.zeros((self.Nn,self.Nn))+100
-	M[self.goal[0],self.goal[1]] = 0
-	#print self.goal
-	#print self.reward
-	#print M
-	t = 0; L = 0
-	while N > L:
-		t += 1
-		for i in range(self.Nn):
-			for j in range(self.Nn):
-				#print i,j, len(reward[i*Nn+j])
-				if(len(self.reward[i][j])==0.0 and M[i,j] == 100.0): L = L + 1; M[i,j] = 1000
-				if(M[i,j] < 100): 
-					for d in range(len(self.reward[i][j])):
-						if(self.reward[i][j][d] == 0):
-							if(M[i+1,j]>M[i,j]+1): 
-								if(M[i+1,j]==100): N = N - 1
-								M[i+1,j] = M[i,j] + 1;
-						if(self.reward[i][j][d] == 1):
-							if(M[i-1,j]>M[i,j]+1): 
-								if(M[i-1,j]==100): N = N - 1
-								M[i-1,j] = M[i,j] + 1;
-						if(self.reward[i][j][d] == 2):
-							if(M[i,j+1]>M[i,j]+1): 
-								if(M[i,j+1]==100): N = N - 1
-								M[i,j+1] = M[i,j] + 1; 
-						if(self.reward[i][j][d] == 3):
-							if(M[i,j-1]>M[i,j]+1): 
-								if(M[i,j-1]==100): N = N - 1
-								M[i,j-1] = M[i,j] + 1; 
-		#print N, L
-	#M[self.goal[0],self.goal[1]] = 100
-	M = np.where(M==1000, 0, M)
-	#print M
-	return M
+					if(len(self.reward[i][j])==0.0 and M[i,j] == 100.0): L = L + 1; M[i,j] = 1000
+					if(M[i,j] < 100): 
+						for d in range(len(self.reward[i][j])):
+							if(self.reward[i][j][d] == 0):
+								if(M[i+1,j]>M[i,j]+1): 
+									if(M[i+1,j]==100): N = N - 1
+									M[i+1,j] = M[i,j] + 1;
+							if(self.reward[i][j][d] == 1):
+								if(M[i-1,j]>M[i,j]+1): 
+									if(M[i-1,j]==100): N = N - 1
+									M[i-1,j] = M[i,j] + 1;
+							if(self.reward[i][j][d] == 2):
+								if(M[i,j+1]>M[i,j]+1): 
+									if(M[i,j+1]==100): N = N - 1
+									M[i,j+1] = M[i,j] + 1; 
+							if(self.reward[i][j][d] == 3):
+								if(M[i,j-1]>M[i,j]+1): 
+									if(M[i,j-1]==100): N = N - 1
+									M[i,j-1] = M[i,j] + 1; 
+		M = np.where(M==1000, 0, M)
+		return M
 
 
 	################################################################################
@@ -222,73 +166,72 @@ class SARSA_evaluation():
 	################################################################################
 
 
-    def visualizationE(self,states,actions):
+	def visualizationE(self,states,actions):
 
-        get_ipython().run_line_magic('matplotlib', 'inline')
+		get_ipython().run_line_magic('matplotlib', 'inline')
 
-        rect1 = patches.Rectangle((self.goal[1]-0.5,self.goal[0]-0.5), 1., 1., color='lime')
-        rect2 = patches.Rectangle((self.start[1]-0.5,self.start[0]-0.5), 1., 1., color='orange')
+		rect1 = patches.Rectangle((self.goal[1]-0.5,self.goal[0]-0.5), 1., 1., color='lime')
+		rect2 = patches.Rectangle((self.start[1]-0.5,self.start[0]-0.5), 1., 1., color='orange')
 
-        plt.figure(figsize=(8,6))
-        plt.title('Number of executed steps: {}'.format(len(self.X)))
-        im = plt.imshow(np.reshape(np.zeros(self.Nn*self.Nn), newshape=(self.Nn,self.Nn)),
-                            interpolation='none', alpha=0.0, vmin=0, vmax=1, aspect='equal');
+		plt.figure(figsize=(8,6))
+		plt.title('Number of executed steps: {}'.format(len(self.X)))
+		im = plt.imshow(np.reshape(np.zeros(self.Nn*self.Nn), newshape=(self.Nn,self.Nn)),
+				interpolation='none', alpha=0.0, vmin=0, vmax=1, aspect='equal' );
 
-        ax = plt.gca();
-        ax = plt.gca();
-			
-        idx = 0
-        for i in range(self.Nn):
-            for j in range(self.Nn):
-                if i > 0:
-                    if(self.actions[idx]):
-                        x1 = (i-1)+0.5; y1 = (j)+0.5;
-                        x2 = (i)-0.5;   y2 = (j)-0.5;
-                        ax.plot([y1,y2],[x1,x2],'k',linestyle='-', linewidth=3)
-                    idx += 1
-                if j > 0:
-                    if(self.actions[idx]):
-                        x1 = (i)+0.5;   y1 = (j-1)+0.5;
-                        x2 = (i)-0.5;   y2 = (j)-0.5;
-                        ax.plot([y1,y2],[x1,x2],'k',linestyle='-', linewidth=3)
-                    idx += 1
+		ax = plt.gca();
+		ax = plt.gca();
 
-
-        for i in range(self.Nn):
-            for j in range(self.Nn):
-                if(states[self.Nn*i+j] == 0.0): 
-                    rectB = patches.Rectangle((j-0.5, i-0.5), 1.0, 1.0, color='black')
-                    ax.add_patch(rectB)
+		idx = 0
+		for i in range(self.Nn):
+		    for j in range(self.Nn):
+			if i > 0:
+			    if(self.actions[idx]):
+				x1 = (i-1)+0.5; y1 = (j)+0.5;
+				x2 = (i)-0.5;   y2 = (j)-0.5;
+				ax.plot([y1,y2],[x1,x2],'k',linestyle='-', linewidth=3)
+			    idx += 1
+			if j > 0:
+			    if(self.actions[idx]):
+				x1 = (i)+0.5;   y1 = (j-1)+0.5;
+				x2 = (i)-0.5;   y2 = (j)-0.5;
+				ax.plot([y1,y2],[x1,x2],'k',linestyle='-', linewidth=3)
+			    idx += 1
 
 
-        ax.set_xlim(-0.5, self.Nn-0.5)
-        ax.set_ylim(-0.5, self.Nn-0.5)
-        ax.invert_yaxis()
+		for i in range(self.Nn):
+		    for j in range(self.Nn):
+			if(states[self.Nn*i+j] == 0.0): 
+			    rectB = patches.Rectangle((j-0.5, i-0.5), 1.0, 1.0, color='black')
+			    ax.add_patch(rectB)
 
 
-        ax.add_patch(rect1)
-        ax.add_patch(rect2)
+		ax.set_xlim(-0.5, self.Nn-0.5)
+		ax.set_ylim(-0.5, self.Nn-0.5)
+		ax.invert_yaxis()
 
-        for hi in range(len(self.X)):
-            rect = patches.Rectangle((self.Y[hi]-0.25,self.X[hi]-0.25),\
-												0.5, 0.5, color='red', alpha=0.05+hi*0.04)
-            ax.add_patch(rect)
 
-        # Major ticks
-        ax.set_xticks(np.arange(0, self.Nn, 1));
-        ax.set_yticks(np.arange(0, self.Nn, 1));
+		ax.add_patch(rect1)
+		ax.add_patch(rect2)
 
-        # Labels for major ticks
-        ax.set_xticklabels(np.arange(0, self.Nn, 1));
-        ax.set_yticklabels(np.arange(0, self.Nn, 1));
+		for hi in range(len(self.X)):
+		    rect = patches.Rectangle((self.Y[hi]-0.25,self.X[hi]-0.25), 0.5, 0.5, color='red', alpha=0.05+hi*0.04)
+		    ax.add_patch(rect)
 
-        # Minor ticks
-        ax.set_xticks(np.arange(-.5, self.Nn, 1), minor=True);
-        ax.set_yticks(np.arange(-.5, self.Nn, 1), minor=True);
+		# Major ticks
+		ax.set_xticks(np.arange(0, self.Nn, 1));
+		ax.set_yticks(np.arange(0, self.Nn, 1));
 
-        # --- Gridlines based on minor ticks
-        ax.grid(which='minor', color='k', alpha = 0.2, linestyle='-', linewidth=1)
+		# Labels for major ticks
+		ax.set_xticklabels(np.arange(0, self.Nn, 1));
+		ax.set_yticklabels(np.arange(0, self.Nn, 1));
 
-        # Gridlines based on minor ticks
-        display.clear_output(wait=True)
-        display.display(plt.gcf())
+		# Minor ticks
+		ax.set_xticks(np.arange(-.5, self.Nn, 1), minor=True);
+		ax.set_yticks(np.arange(-.5, self.Nn, 1), minor=True);
+
+		# --- Gridlines based on minor ticks
+		ax.grid(which='minor', color='k', alpha = 0.2, linestyle='-', linewidth=1)
+
+		# Gridlines based on minor ticks
+		display.clear_output(wait=True)
+		display.display(plt.gcf())
