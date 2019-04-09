6ebb36147454d54112676dabea34b6bc69c1412e
Ihor Kuras <ihor.kuras@epfl.ch>
2019-04-09 08:45:03 +0200

    Update SARSA_additional.py

--- epflx_robox_nrp_utils/SARSA/SARSA_additional.py
+++ epflx_robox_nrp_utils/SARSA/SARSA_additional.py
@@ -618,10 +618,10 @@ class SARSA_additional():
 		return self.pos
 
 
-	def upload_lattice(self,Lfile="SOM_data_lattice.csv"):
+	def upload_lattice(self,Lfile='SOM_data_lattice.csv'):
 		# load data of som-lattice from csv 
 		script_path = os.path.dirname(os.path.abspath( __file__ ))
-		with open(os.path.join(script_path,'../graduation/SOM_test1_lattice.csv')) as f:
+		with open(os.path.join(script_path,'../graduation/'+Lfile)) as f:
 			reader = csv.reader(f)
 			next(reader) # skip header
 			data = [r for r in reader]
